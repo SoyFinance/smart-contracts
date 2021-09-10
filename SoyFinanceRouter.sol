4c61cd7f0a473e46ee0247ab9877618624c60ec3
Yuriy <39490997+yuriy77k@users.noreply.github.com>
2021-09-10 15:06:30 +0300

    Update SoyFinanceRouter.sol

--- SoyFinanceRouter.sol
+++ SoyFinanceRouter.sol
@@ -303,7 +303,7 @@ library SoyFinanceLibrary {
     // fetches and sorts the reserves for a pair
     function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
         (address token0,) = sortTokens(tokenA, tokenB);
-        pairFor(factory, tokenA, tokenB);
+        
         (uint reserve0, uint reserve1,) = ISoyFinancePair(pairFor(factory, tokenA, tokenB)).getReserves();
         (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
